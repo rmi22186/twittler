fd2fc8b806932c29b17ec8d2d7d53df9d34f2a30
Robert Ing <rmi22186@gmail.com>
2014-11-15 19:05:13 -0800

    edit data_generator to allow for date creation for user tweets, modify user tweet length to be max 140 characters, use writetweet from data_generator

--- javascript.js
+++ javascript.js
@@ -1,10 +1,19 @@
 $(document).ready(function(){
-  var $tweetContainer = $('.tweet-container ');
+  var $tweetContainer = $('.tweet-container');
+  var setTweetInterval = 0;                               // declaring setTweetInterval
+  var waitTime = 5000;
+  var setTweetIntervalAllUserRefresh = function() {
+    setTweetInterval = window.setInterval(function() {    // set the function to run refreshHomePageTweets every x seconds
+        refreshHomePageTweets();                                  
+      }, waitTime);
+  };
   
+  setTweetIntervalAllUserRefresh();                       // run this function on page load
+
   //refresh tweets function
   var refreshHomePageTweets = function() {                // set the initial function
     $('.refreshAllTweets').remove();
-    $tweetContainer.html('')                                         // clear the page when refreshHomePageTweets is called
+    $tweetContainer.html('')                              // clear the page when refreshHomePageTweets is called
     var index = streams.home.length - 1;                  // 
     while(index >= 0){                                  
       var tweet = streams.home[index];
@@ -19,66 +28,46 @@ $(document).ready(function(){
     }}
   refreshHomePageTweets();                                // call refreshHomePageTweets as page loads
 
-  var setTweetInterval = 0;                               // declaring setTweetInterval
-  var determineWhichTweets = function() {                 // 
-    if ($('body').hasClass("all-tweets")) {               // if the body has a class of 'all-tweets', which it does when the page is first loaded
-      setTweetInterval = window.setInterval(function() {  // set the function to run refreshHomePageTweets every x seconds
-        refreshHomePageTweets();                                  
-      }, 5000); 
-    }
-  }
-  determineWhichTweets();                                 // run this function on page load
-  
-  $('.tweet-container').on('click', 'div', function() {                     // when a user name is clicked
+  //click on user name and show only user tweets
+  $('.tweet-container').on('click', 'div', function() {   // when a user name is clicked
     var clickedClass = $(this).attr('id');                // the attribute of the div is stored in variable clickedClass
-    console.log(clickedClass + " clicked!")
-    $('.refreshAllTweets').remove();
     $tweetContainer.html('');                             // everything in the nav html is cleared
 
     clearInterval (setTweetInterval);                     // the setTweetInterval is cleared, so all users are no longer being refreshed
-    
     setTweetInterval = window.setInterval(function() {    // setTweetInterval now runs chooseUserStream, which shows just that user's stream
       chooseUserStream();
-    }, 5000);
+    }, waitTime);
     
+    $('.refreshAllTweets').remove();
     var $userButton = $('<button class="btn-success btn refreshAllTweets card-similar"><span class="glyphicon glyphicon-chevron-left"></span></button>')
     $userButton.appendTo('.middle-column')                                          
 
-  var chooseUserStream = function() {                 
-    $tweetContainer.html('');                                      // when chooseUserStream is called, clear the nav
-    var index = streams.users[clickedClass].length -1;  // set index to array length of all of that users Tweets (-1 for array.length 0 index property), setting the index to the end of the array means the latest tweets are rendered at the top of the page
-    while(index >= 0) {                                 // while the index is greater than or equal to 0
-      var tweet = streams.users[clickedClass][index];   // set var tweet to the specific tweet
-      var $tweet = $('<div class="card"></div>');                    // set var $tweet to an empty div
-      $tweet.attr('id', clickedClass);                    // add a class of the username to the div
-      $tweet.html(  '<h4>@</h4>' + 
-                    '<b>' + '<h2>' + tweet.user + '</h2>' + '</b><br>' + 
-                    tweet.message +
-                    ' <i class="timestamp">'+ $.timeago(tweet.created_at) + '</i>');
-      $tweet.appendTo($tweetContainer);                 // append tweets to the nav
-      index -= 1;                                       // reduce index by 1
-      
+    var chooseUserStream = function() {                 
+      $tweetContainer.html('');                             // when chooseUserStream is called, clear the nav
+      var index = streams.users[clickedClass].length -1;    // set index to array length of all of that users Tweets (-1 for array.length 0 index property), setting the index to the end of the array means the latest tweets are rendered at the top of the page
+      while(index >= 0) {                                   // while the index is greater than or equal to 0
+        var tweet = streams.users[clickedClass][index];     // set var tweet to the specific tweet
+        var $tweet = $('<div class="card"></div>');         // set var $tweet to an empty div
+        $tweet.attr('id', clickedClass);                    // add a class of the username to the div
+        $tweet.html(  '<h4>@</h4>' + 
+                      '<b>' + '<h2>' + tweet.user + '</h2>' + '</b><br>' + 
+                      tweet.message +
+                      ' <i class="timestamp">'+ $.timeago(tweet.created_at) + '</i>');
+        $tweet.appendTo($tweetContainer);                   // append tweets to the nav
+        index -= 1;                                         // reduce index by 1   
+      }
     }
-  }
-  
-  chooseUserStream();                                   // call the chooseUserStream function when nav is clicked
-  
+    chooseUserStream();                                     // call the chooseUserStream function when nav is clicked
   });
 
   $('.middle-column').on('click', '.refreshAllTweets', function() {
-    console.log('refreshAllTweets clicked!')
     clearInterval (setTweetInterval);
-    setTweetInterval = window.setInterval(function() {    
-      refreshHomePageTweets();
-    }, 5000);
+    setTweetIntervalAllUserRefresh();
     refreshHomePageTweets();
   })
 
-
-  var newPerson = prompt("What hande would you like to use?", 'Enter your username here')
-  console.log(newPerson)
-  streams.users[newPerson] = [];
-  streams.home = [];
+  visitor = prompt("What hande would you like to use?", 'Enter your username here')
+  streams.users[visitor] = [];
 
   $('#tweet-form').keypress(function(e) {
       if (event.which == 13 ) {
@@ -87,17 +76,14 @@ $(document).ready(function(){
       }
   });
 
-  $('.submit-tweet').on('click', function() {
-    var submittedTweet = $('#tweet-form').val();
-    $('#tweet-form').val('');
-    var tweet = {};
-    tweet.message = submittedTweet;
-    tweet.user = newPerson;
-    tweet.created_at = new Date();
-    var addNewTweet = function(tweet) {  
-      streams.home.push(tweet);
-      streams.users[newPerson].push(tweet);
-    };
-    addNewTweet(tweet);
+  $('.submit-tweet').on('click', function() {             // when submit tweet is clicked on
+    var submittedTweet = $('#tweet-form').val();          // assign tweet string to variable
+    if (submittedTweet.length === 0) {                    // if nothing typed into form, alert user
+      alert('you need to type something!')
+    }
+    else {
+      writeTweet(submittedTweet);
+    }
+    $('#tweet-form').val('');                             // set value to default
   })    
 })
